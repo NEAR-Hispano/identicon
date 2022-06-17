e22801e0d561552aa37be3f1410c7b0d8fade123
Mario Zito <mario.zito@treme.io>
2022-06-17 01:34:46 -0300

    Cleaning and reformat

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -9,30 +9,29 @@ mod requests;
 // mod payments;
 // mod upgrades;
 // mod validators;
-
-//#[cfg(test)]
 mod tests;
+
 
 #[near_bindgen]
 impl VerificationContract {
-    #[init]
-    pub fn new() -> Self {
-        assert!(!env::state_exists(), "Already initialized");
-        log!("\nnew() Initialized contract state");
-        Self {
-            verifications: UnorderedMap::new(b"a"),
-            subjects: UnorderedMap::new(b"b"),
-            assignments: UnorderedMap::new(b"c"),
-            spendings: UnorderedMap::new(b"d"),
-            validators: Vec::new(),
-            params: Parameters {
-                min_validators_needed: 2,
-                max_reviewers_needed: 1,
-                remote_validation_fee: 1.0,
-                onsite_validation_fee: 1.0,
-                review_validation_fee: 1.0,
-                allowed_monthly_requests: 2,
-            },
-        }
+  #[init]
+  pub fn new() -> Self {
+    assert!(!env::state_exists(), "Already initialized");
+    log!("\nnew VerificationContract: Initialized contract state");
+    Self {
+      verifications: UnorderedMap::new(b"a"),
+      subjects: UnorderedMap::new(b"b"),
+      assignments: UnorderedMap::new(b"c"),
+      spendings: UnorderedMap::new(b"d"),
+      validators: Vec::new(),
+      params: Parameters {
+          min_validators_needed: 2,
+          max_reviewers_needed: 1,
+          remote_validation_fee: 1.0,
+          onsite_validation_fee: 1.0,
+          review_validation_fee: 1.0,
+          allowed_monthly_requests: 2,
+      },
     }
+  }
 }
