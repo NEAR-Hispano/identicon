58939ccc4d462dee7108ace02937bb83ce2674a8
Leandro Manzanal <leomanzanal@gmail.com>
2022-06-23 09:31:21 -0300

    update contract methods constants

--- gateway/src/services/contractMethods.js
+++ gateway/src/services/contractMethods.js
@@ -5,7 +5,9 @@ const viewMethods = {
 const changeMethods = {
     requestVerification: 'request_verification',
     cancelVerification: 'cancel_verification',
-    registerAsValidator: 'register_as_validator'
+    registerAsValidator: 'register_as_validator',
+    unregisterAsValidator: 'unregister_as_validator',
+    assignValidators: 'assign_validators'
 }
 
 module.exports = {
